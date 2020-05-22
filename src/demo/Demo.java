0f34135f257825ec2fb3442b1490328402e5a7a6
Diep Nguyen <diepniit.nguyenhong@gmail.com>
2020-05-22 08:37:44 +0700

    Update miscellaneous files

---
+++ src/demo/Demo.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.util.Random;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt(1));
+	}
+
+}
